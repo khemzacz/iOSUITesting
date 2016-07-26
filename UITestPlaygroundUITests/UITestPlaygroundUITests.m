ad5723ec6e6339065ae1f77141e5635a4439626b
khemzaczek <konrad622@gmail.com>
2016-07-26 16:45:39 +0200

    - 2nd commit

--- UITestPlaygroundUITests/UITestPlaygroundUITests.m
+++ UITestPlaygroundUITests/UITestPlaygroundUITests.m
@@ -16,7 +16,7 @@
 
 - (void)setUp {
     [super setUp];
-    
+    //change
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
     // In UI tests it is usually best to stop immediately when a failure occurs.
